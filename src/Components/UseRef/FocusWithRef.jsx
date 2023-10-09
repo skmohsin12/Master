14e14d4d2be96449ab4f365c0b180acd3b6dc99a
Shaikh Mohsin <mr.skmohsin12@gmail.com>
2023-10-09 19:15:31 +0545

    one comment added

--- src/Components/UseRef/FocusWithRef.jsx
+++ src/Components/UseRef/FocusWithRef.jsx
@@ -1,5 +1,8 @@
 import React, { useRef, useEffect } from "react";
 
+
+//useRef() hook is used to access DOM elements directly.
+//For example - tell the below example useRef() is used for preference purpose and focus on the input field when page loads.
 export const FocusRef = () => {
   const inputRef = useRef(null);
 
